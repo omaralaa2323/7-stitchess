f3d3abf0ab54b905efb8ab47ed0aa857ba0c0149
omaralaa2323 <omaralaa2413@gmail.com>
2025-06-21 23:10:51 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,1 +1,12 @@
-// App component entry point
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>🧶 7 Stitchess is Live!</h1>
+      <p>Your handmade crochet store is ready to go.</p>
+    </div>
+  );
+}
+
+export default App;
